53fbed8914c35bced72177380a4eb8abdf16d8d4
nishaddesai97 <nishaddesai1997@gmail.com>
2023-08-25 19:27:53 +0530

    added div

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import Footer from './Atoms/Footer';
 import Header from './Atoms/Header';
@@ -98,91 +98,92 @@ const Account = () => {
         }
     }
     return (
-        <section>
+        <section >
             <Header />
-            <section className='px-16 relative'>
-             
-                <div className='border-x border-black py-10' >
+            {/* <div className='relative w-[65px] h-[65px] border-r border-b border-black ' ></div>
+            <div className='relative w-[65px] h-[65px] relative border-l border-b border-black left-[1635px] bottom-[65px]' ></div> */}
+            <div className='flex justify-between'>
+                <div class="flex w-16 h-16 border-r border-black">
                   
-                    <div className='w-full border-t h-auto flex  border-black' ></div>
+                </div>
+                <div class="flex w-16 h-16 border-l border-black place-self-end">
+                  
+                </div>
+            </div>
+            <section className='px-16  border-t border-black   '>
+                <div className='w-full p-4 text-start text-2xl space-x-7 border-x border-black' >
+                    <a className='uppercase text-[#838282]  w-full font-Staatliches' href="">orders</a>
+                    <a className='uppercase text-[#838282]    w-full font-Staatliches' href="">subscriptions</a>
+                    <a className='uppercase text-[#838282]    w-full font-Staatliches' href="">golden ticket</a>
+                    <a className='uppercase text-black w-full font-Staatliches underline decoration-[6px]  underline-offset-[18px]' href="">account</a>
+                </div>
 
-                    <div className='w-full p-4 text-start text-2xl space-x-7' >
-                        <a className='uppercase text-[#838282]  w-full font-Staatliches' href="">orders</a>
-                        <a className='uppercase text-[#838282]    w-full font-Staatliches' href="">subscriptions</a>
-                        <a className='uppercase text-[#838282]    w-full font-Staatliches' href="">golden ticket</a>
-                        <a className='uppercase text-black w-full font-Staatliches underline decoration-[6px]  underline-offset-[18px]' href="">account</a> 
-                    </div>
-
-                    <div className='w-full border-t h-auto flex   border-b border-black'>
-                        <div className='w-full flex flex-col border-black py-6'>
-                            <p className='px-6 text-5xl h-auto font-Staatliches flex items-center justify-start'>MY ACCOUNT</p>
-                            <p className='px-6 w-full border-black flex flex-col justify-evenly text-start'>Personalize your experience here and see everything we track and do with your data.</p>
-
-                        </div>
-                        <div className='w-80 flex gap-3 p-8 place-content-end' >
-                            <img className='h-5 w-5' src={pencil} alt="bag" />
-                            <p className='text-orange-400 font-Staatliches text-xl '>
-                                EDIT DETAILS
-                            </p>
-                        </div>
+                <div className='w-full border-t h-auto flex   border-x border-b border-black'>
+                    <div className='w-full flex flex-col border-black py-6'>
+                        <p className='px-6 text-5xl h-auto font-Staatliches flex items-center justify-start'>MY ACCOUNT</p>
+                        <p className='px-6 w-full border-black flex flex-col justify-evenly text-start'>Personalize your experience here and see everything we track and do with your data.</p>
 
                     </div>
+                    <div className='w-80 flex gap-3 p-8 place-content-end' >
+                        <img className='h-5 w-5' src={pencil} alt="bag" />
+                        <p className='text-orange-400 font-Staatliches text-xl '>
+                            EDIT DETAILS
+                        </p>
+                    </div>
 
+                </div>
 
-                    {/* subpart of who we are */}
-                    <div className='w-full h-auto flex justify-center border-b border-black'>
-                        <ul className='h-auto flex flex-col items-start p-6 '>
-                            <div>
-                                <img className=" w-full object-cover h-40" src={user} title="poster" />
-                            </div>
-                        </ul>
-                        <div className='w-full h-auto p-6 flex flex-col'>
-                            <section className='w-full h-auto'>
-                                <form className='flex-col flex items-start gap-5 capitalize' action="/" method="get" onSubmit={handleEdit}>
-                                    <div className='w-full flex gap-5'>
-                                        <div className='w-full text-start'>
-                                            <input className={`w-full py-2 pl-4  border-2 ${formErrors.first_name ? 'border-red-500' : 'border-gray-400'
-                                                }`} placeholder='Enter first name' onChange={handleChange} type="text" name="first_name" id="ufirstname" />
-                                            <p className='text-red-500 text-sm'>{formErrors.first_name}</p>
-                                        </div>
-                                        <div className='w-full text-start'>
-                                            <input className={`w-full  py-2 pl-4  border-2 ${formErrors.last_name ? 'border-red-500' : 'border-gray-400'
-                                                }`} placeholder='Enter last name' onChange={handleChange} type="text" name="last_name" id="ulastname" />
-                                            <p className='text-red-500 text-sm'>{formErrors.last_name}</p>
-                                        </div>
-                                    </div>
-                                    <div className='w-full text-start'>
-                                        <input className={`w-full py-2 pl-4 border-2  ${formErrors.email ? 'border-red-500' : 'border-gray-400'
-                                            }`} placeholder='Email address' onChange={handleChange} type="email" name="email" id="umail" />
-                                        <p className='text-red-500 text-sm'>{formErrors.email}</p>
-                                    </div>
-                                    <div className='w-full text-start'>
-
-                                        <input className={`w-full py-2 pl-4 border-2  ${formErrors.mobile ? 'border-red-500' : 'border-gray-400'
-                                            }`} placeholder='Phone number' onChange={handleChange} type="tel" name="mobile" id="umobile" />
-                                        <p className='text-red-500 text-sm'>{formErrors.mobile}</p>
-                                    </div>
-                                    <div className='w-full text-start'>
-
-                                        <textarea className={`w-full py-2 pl-4 border-2  ${formErrors.address ? 'border-red-500' : 'border-gray-400'
-                                            }`} placeholder='Address' onChange={handleChange} type="textarea" name="address" id="uaddress" />
-                                        <p className='text-red-500 text-sm'>{formErrors.address}</p>
-                                    </div>
-                                    <div className='flex gap-5'>
-
-                                        <input className=' px-6 py-3 text-white font-Staatliches  bg-black' type="submit" value="SAVE CHANGES" />
-                                        <input className=' px-6 py-3 text-black font-Staatliches border-2 border-gray-900 ' type="reset" value="DISCARD" />
-                                    </div>
-
-
-                                </form>
-                            </section>
+                <div className='w-full h-auto flex justify-center border-b border-x border-black'>
+                    <ul className='h-auto flex flex-col items-start p-6 '>
+                        <div>
+                            <img className=" w-full object-cover h-40" src={user} title="poster" />
                         </div>
+                    </ul>
+                    <div className='w-full h-auto p-6 flex flex-col'>
+                        <section className='w-full h-auto'>
+                            <form className='flex-col flex items-start gap-5 capitalize' action="/" method="get" onSubmit={handleEdit}>
+                                <div className='w-full flex gap-5'>
+                                    <div className='w-full text-start'>
+                                        <input className={`w-full py-2 pl-4  border-2 ${formErrors.first_name ? 'border-red-500' : 'border-gray-400'
+                                            }`} placeholder='Enter first name' onChange={handleChange} type="text" name="first_name" id="ufirstname" />
+                                        <p className='text-red-500 text-sm'>{formErrors.first_name}</p>
+                                    </div>
+                                    <div className='w-full text-start'>
+                                        <input className={`w-full  py-2 pl-4  border-2 ${formErrors.last_name ? 'border-red-500' : 'border-gray-400'
+                                            }`} placeholder='Enter last name' onChange={handleChange} type="text" name="last_name" id="ulastname" />
+                                        <p className='text-red-500 text-sm'>{formErrors.last_name}</p>
+                                    </div>
+                                </div>
+                                <div className='w-full text-start'>
+                                    <input className={`w-full py-2 pl-4 border-2  ${formErrors.email ? 'border-red-500' : 'border-gray-400'
+                                        }`} placeholder='Email address' onChange={handleChange} type="email" name="email" id="umail" />
+                                    <p className='text-red-500 text-sm'>{formErrors.email}</p>
+                                </div>
+                                <div className='w-full text-start'>
+
+                                    <input className={`w-full py-2 pl-4 border-2  ${formErrors.mobile ? 'border-red-500' : 'border-gray-400'
+                                        }`} placeholder='Phone number' onChange={handleChange} type="tel" name="mobile" id="umobile" />
+                                    <p className='text-red-500 text-sm'>{formErrors.mobile}</p>
+                                </div>
+                                <div className='w-full text-start'>
+
+                                    <textarea className={`w-full py-2 pl-4 border-2  ${formErrors.address ? 'border-red-500' : 'border-gray-400'
+                                        }`} placeholder='Address' onChange={handleChange} type="textarea" name="address" id="uaddress" />
+                                    <p className='text-red-500 text-sm'>{formErrors.address}</p>
+                                </div>
+                                <div className='flex gap-5'>
+
+                                    <input className=' px-6 py-3 text-white font-Staatliches  bg-black' type="submit" value="SAVE CHANGES" />
+                                    <input className=' px-6 py-3 text-black font-Staatliches border-2 border-gray-900 ' type="reset" value="DISCARD" />
+                                </div>
+
+
+                            </form>
+                        </section>
                     </div>
                 </div>
+
             </section>
-
-
             <Footer />
         </section >
 
